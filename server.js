2c5453a5bbf29bbc81d40ac390f4ddd057c8a20c
TEpolito <tepolito55@gmail.com>
2017-11-29 10:39:55 -0500

    completed assignment

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 // call `ShoppingList.update` with updated item.
 app.put('/shopping-list/:id', jsonParser, (req, res) => {
   const requiredFields = ['name', 'budget', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
+  for (let i=0; i<requiredFields.length; i++) 
+  {
     const field = requiredFields[i];
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
@@ -110,6 +111,33 @@ app.delete('/recipes/:id', (req, res) => {
   console.log(`Deleted recipe \`${req.params.ID}\``);
   res.status(204).end();
 });
+
+app.put('/recipes/:id', jsonParser, (req, res) =>
+{
+  const requiredFields = ['name','id','ingredients'];
+  for(let i=0; i<requiredFields.length;i++)
+  {
+    const field = requiredFields[i];
+
+    if(!(field in req.body))
+    {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+
+  }
+
+  if (req.params.id !== req.body.id) 
+  {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+
+  Recipes.update({id: req.params.id, name: req.name, ingredients: req.ingredients});
+  res.status(204).end();
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
